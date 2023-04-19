810884d905573e8090d23be5339cf1ea70c8d599
MyFnf <113801267+LilDrippyMyFnf@users.noreply.github.com>
2023-04-19 16:29:25 -0400

    Revert "prefer logs"
    This reverts commit 101f410f182e13170a1f318db5665a6a3794bd08.

--- source/GameplayChangersSubstate.hx
+++ source/GameplayChangersSubstate.hx
@@ -94,9 +94,6 @@ class GameplayChangersSubstate extends MusicBeatSubstate
 
 		var option:GameplayOption = new GameplayOption('Botplay', 'botplay', 'bool', false);
 		optionsArray.push(option);
-		
-		var option:GameplayOption = new GameplayOption('Show Time Bar?', 'timeBarType', 'string', Disabled); // idk how to fix it, xd
-		optionsArray.push(option);
 	}
 
 	public function getOptionByName(name:String)
